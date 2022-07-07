f588174590e97d88227967d618fe079438fcf6fa
EC2 Default User <ec2-user@ip-172-31-23-112.us-west-1.compute.internal>
2022-07-07 19:55:42 +0000

    演算子

--- main2.js
+++ main2.js
@@ -1,23 +1,16 @@
-
-
-
-
-
-
-
-
-
-function clickbutton (button){
 let result = document.getElementById("result");
 
+function clickbutton (button){
+ let target_value = button.innerHTML;
 /*問題なくconsole.logは動く
 console.log(button) ;
  console.log(result);
  result.innerHTML += result.innerHTML
 */
 
-    let target_value =button.innerHTML;
-    if(target_value == "AC"){
+   
+
+    if(target_value =="AC"){
         console.log("クリア")
         result.innerHTML ="0"
         console.log("0");
@@ -33,13 +26,36 @@ console.log(button) ;
             result.innerHTML += target_value
             console.log("計算完了");
         }
-}
-
-}
+       
 
 
+//const operate = 　document.getElementsByClassName('operator')←演算子のクラス名;
+let operate =　document.getElementsByClassName('operate')
+ function edit (button){
+ if (result.innerHTML.slice(-1) === "+" ){
+            
+          
+           return;
+           
+        }else if(result.innerHTML.slice(-1) === "-" ){
+            return;
+        }
+        else if(result.innerHTML.slice(-1) === "*" ){
+             return;
+        }
+          else if(result.innerHTML.slice(-1) === "/" ){
+              return; 
+              console.log("クリア")
+          }else{
+            result.innerHTML += target_value
+            console.log("計算完了");
+        }
+       
+}}}
 
-
+//let operate  = result.innerHTML;
+// 末尾から2文字を削除
+//var b = str.slice( 0, -2 ) ;
 
 //let get_calc = function (button){
 //console.log("button.value");
